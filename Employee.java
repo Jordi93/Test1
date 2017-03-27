0b54e19d20820140573343197f5b738faa7727c6
Jordi93 <jordi.romani.vidal@gmail.com>
2017-03-27 16:35:15 +0200

    penjo lo de clase

---
+++ Employee.java
@@ -0,0 +1,16 @@
+package hands_on_2;
+
+public class Employee {
+	String name;
+	double base_pay;
+	double hours_worked;
+	
+	public Employee(){
+		this.name;
+		this.base_pay;
+		this.hours_worked;
+	}
+	public void Salary() {
+		System.out.println(base_pay*hours_worked);
+	}
+}
---
+++ FooCorporation.java
@@ -0,0 +1,7 @@
+package hands_on_2;
+
+public class FooCorporation {
+	public static void main (String[] args){
+		Employee John = new Employee()
+}
+}
